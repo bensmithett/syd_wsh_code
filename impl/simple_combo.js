953fd64c2fa312376e59188e64d87ae2091ab69d
Axel Rauschmayer <axel@rauschma.de>
2013-10-21 17:03:13 +1100

    Better explanation for simple_test.js

--- impl/simple_combo.js
+++ impl/simple_combo.js
@@ -1,5 +1,5 @@
-var simple_m = require('../support/simple_module');
 var spaceit = require('../support/simple_spaceit');
+var simple_m = require('../support/simple_module');
 
 function reverseSpace(str) {
     return spaceit(simple_m.reverse('hello'));
--- test/simple_test.js
+++ test/simple_test.js
@@ -1,14 +1,18 @@
 var assert = require('assert');
 
-var simple_m = require('../support/simple_module');
 var spaceit = require('../support/simple_spaceit');
-// Implement the following module, using the previous two
+var simple_m = require('../support/simple_module');
+
+// EXERCISE: implement the following module, using the previous two
 var combo = require('../impl/simple_combo');
 
+//// Test module simple_spaceit
 assert.strictEqual(spaceit('hello'), 'h e l l o');
 assert.strictEqual(spaceit('x'), 'x');
 
+//// Test module simple_module
 assert.strictEqual(simple_m.reverse('hello'), 'olleh');
 
+//// Test module simple_combo
 // reverseSpace() combines the above two functions
 assert.strictEqual(combo.reverseSpace('hello'), 'o l l e h');
